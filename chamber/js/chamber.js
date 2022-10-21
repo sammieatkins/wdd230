18da0b3eb8a0fa9ad4a29149fd62791db9929acb
sammieatkins <atk21007@byui.edu>
2022-10-21 10:21:41 -0600

    class

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -21,6 +21,17 @@ if (now.getDay() == 1 || now.getDay() == 2) {
     parent.style.display = "block";
 }
 
+// temp, speed, chill
+// weather stuff
+const temperature = document.querySelector("#temp").textContent;
+const windSpeed = document.querySelector("#speed").textContent;
+
+if (temperature < 50 && windSpeed > 3) {
+    let chill = Math.round((35.74 + (0.6215 * temperature))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275*temperature*Math.pow(windSpeed,0.16)));
+    const windChill = document.querySelector("#chill").textContent;
+    windChill.innerHTML = chill + "&#8457;";
+}
+
 // menu click event
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
