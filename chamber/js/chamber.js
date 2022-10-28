237553a9ec910c2d23cb2c2b4de5b16729dfe94a
sammieatkins <atk21007@byui.edu>
2022-10-28 16:33:20 -0600

    more caption editing

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -23,59 +23,49 @@ if (now.getDay() == 1 || now.getDay() == 2) {
 }
 
 
-// // weather stuff
-// const temperature = document.querySelector("#temp").textContent;
-// const windSpeed = document.querySelector("#speed").textContent;
-
-// if (temperature < 50 && windSpeed > 3) {
-//     const chill = Math.round((35.74 + (0.6215 * temperature))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275 * temperature * Math.pow(windSpeed,0.16)));
-//     document.querySelector("#chill").innerHTML = chill + "&#8457;";
-// }
-
-
 // lazy load
 const image = document.querySelectorAll('img');
 
 const pimages = document.querySelectorAll('[data-src]');
 const options = {
-threshold: 1,
+    threshold: 1,
 rootMargin: "0px 0px 100px 0px"
 }
 
 function preloadImage(img){
-const source = img.getAttribute('data-src');
-if (!source) {
-return;
-}
-
-img.src = source;
+    const source = img.getAttribute('data-src');
+    if (!source) {
+        return;
+    }
+    
+    img.src = source;
 }
 
 const io = new IntersectionObserver (
-(entries, io) => {
-entries.forEach(entry => {
-if (!entry.isIntersecting) {
-return;
-} else {
-preloadImage(entry.target);
-io.unobserve(entry.target);
-}
-});
-},options);
-
-pimages.forEach(image => {
-io.observe(image);
-})
-
-
-// date since last visit
-let currDate = Date.now();
-
-console.log(currDate);
-
-let visitMessage = document.querySelector("#visitMessage");
-let daysSince = document.querySelector("#daysSince");
-
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+        });
+    },options);
+    
+    pimages.forEach(image => {
+        io.observe(image);
+    })
+    
+    
+    // date since last visit
+    let currDate = Date.now();
+    
+    console.log(currDate);
+    
+    let visitMessage = document.querySelector("#visitMessage");
+    let daysSince = document.querySelector("#daysSince");
+    
 if (!localStorage.getItem("visitDate"))
 {
     localStorage.setItem("visitDate", currDate);
@@ -84,7 +74,7 @@ if (!localStorage.getItem("visitDate"))
 else
 {
     let prevDate = localStorage.getItem("visitDate");
-
+    
     let difference = currDate - prevDate;
     // console.log(difference);
     let daysBtwn = Math.floor(difference / 1000 / 60 / 60 / 24);
@@ -98,5 +88,15 @@ else
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
 }
-const x = querySelector("#hamburgerBtn");
+const x = document.querySelector("#hamburgerBtn");
 x.onclick = toggleMenu;
+
+
+// weather stuff
+const temperature = document.querySelector("#temp").textContent;
+const windSpeed = document.querySelector("#speed").textContent;
+
+if (temperature < 50 && windSpeed > 3) {
+    const chill = Math.round((35.74 + (0.6215 * temperature))-(35.75 * Math.pow(windSpeed,0.16)) + (0.4275 * temperature * Math.pow(windSpeed,0.16)));
+    document.querySelector("#chill").innerHTML = chill + "&#8457;";
+}
