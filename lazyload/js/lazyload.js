9594aea0d27813f7243da7b557e4baa2e24501e1
sammieatkins <atk21007@byui.edu>
2022-10-27 18:09:05 -0600

    lazy load and updated portal

---
+++ lazyload/js/lazyload.js
@@ -0,0 +1,35 @@
+const image = document.querySelectorAll("img");
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            }
+            else {
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+// [] for targeting attributes
+const pimages = document.querySelectorAll("[data-src]");
+const options = {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px"
+}
+
+function preloadImage(img) 
+{
+    const source = img.getAttribute("data-src");
+    if (!source) {
+        return;
+    }
+    
+    img.src = source;
+}
+
+pimages.forEach(image => {
+    io.observe(image);
+})
