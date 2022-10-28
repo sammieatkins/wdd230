4bdbd5bf5700bc38308d43625bac50ef250310cd
sammieatkins <atk21007@byui.edu>
2022-10-28 14:47:26 -0600

    lazy load and images for discover page

--- lazyload/js/lazyload2.js
+++ lazyload/js/lazyload2.js
@@ -45,3 +45,41 @@ io.unobserve(entry.target);
 pimages.forEach(image => {
 io.observe(image);
 })
+
+
+// localStorage
+
+// localStorage.setItem("msg1", "Hello Permanent");
+
+// sessionStorage.setItem("msg2", "Hello Temporary");
+
+
+
+// difference in days between two dates (or last time they visited)
+
+// <h1 id="visitMessage">Days since last visit: <span id="daysSince"></span></h1>
+
+let currDate = Date.now();
+// let currDate = new Date("2022-03-24");
+console.log(currDate);
+// let visitDate = localStorage.setItem("visitDate", currDate);
+
+let visitMessage = document.querySelector("#visitMessage");
+let daysSince = document.querySelector("#daysSince");
+
+if (!localStorage.getItem("visitDate"))
+{
+    localStorage.setItem("visitDate", currDate);
+    visitMessage.textContent = "This is your first visit."
+}
+else
+{
+    let prevDate = localStorage.getItem("visitDate");
+
+    let difference = currDate - prevDate;
+    // console.log(difference);
+    let daysBtwn = Math.floor(difference / 1000 / 60 / 60 / 24);
+    daysSince.textContent = daysBtwn;
+    // console.log(daysBtwn);
+    localStorage.setItem("visitDate", currDate);
+}
