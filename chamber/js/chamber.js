915b6dc671ecc830057b41dac28f7c502b71b107
sammieatkins <atk21007@byui.edu>
2022-10-31 10:21:56 -0600

    final

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -66,22 +66,22 @@ const io = new IntersectionObserver (
     let visitMessage = document.querySelector("#visitMessage");
     let daysSince = document.querySelector("#daysSince");
     
-if (!localStorage.getItem("visitDate"))
-{
-    localStorage.setItem("visitDate", currDate);
-    visitMessage.textContent = "This is your first visit."
-}
-else
-{
-    let prevDate = localStorage.getItem("visitDate");
-    
-    let difference = currDate - prevDate;
-    // console.log(difference);
-    let daysBtwn = Math.floor(difference / 1000 / 60 / 60 / 24);
-    daysSince.textContent = daysBtwn;
-    // console.log(daysBtwn);
-    localStorage.setItem("visitDate", currDate);
-}
+    if (!localStorage.getItem("visitDate"))
+    {
+        localStorage.setItem("visitDate", currDate);
+        visitMessage.textContent = "This is your first visit."
+    }
+    else
+    {
+        let prevDate = localStorage.getItem("visitDate");
+        
+        let difference = currDate - prevDate;
+        // console.log(difference);
+        let daysBtwn = Math.floor(difference / 1000 / 60 / 60 / 24);
+        daysSince.textContent = daysBtwn;
+        // console.log(daysBtwn);
+        localStorage.setItem("visitDate", currDate);
+    }
 
 
 // menu click event
