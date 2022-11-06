8b80eecfe274ccc6bfab9a06dbf8dcdfd5c3ba01
sammieatkins <atk21007@byui.edu>
2022-11-05 23:09:55 -0600

    join page

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -83,6 +83,17 @@ else
     localStorage.setItem("visitDate", currDate);
 }
 
+// join page
+const date = new Date();
+const dateTime = {
+    "month": date.getMonth(),
+    "date": date.getDate(),
+    "year": date.getFullYear(),
+    "hours": date.getHours(),
+    "minutes": date.getMinutes(),
+    "seconds": date.getSeconds()
+};
+
 
 // menu click event
 function toggleMenu() {
