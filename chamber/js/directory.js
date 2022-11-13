18414439575195c64a71ff002f553b28e006e0b3
sammieatkins <atk21007@byui.edu>
2022-11-12 19:22:50 -0700

    data and setup directory page

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,16 +1,52 @@
 // directory
 // -- event listeners on table button and card button
 // -- get rid of table/cards as well
-const requestURL = "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+const requestURL = "https://sammieatkins.github.io/wdd230/chamber/data.json";
 
-async function getProphets(requestURL) {
+async function getBusinesses(requestURL) {
     const response = await fetch(requestURL);
     console.log(response);
     if (response.ok) {
         const jsObject = await response.json();
         console.log(jsObject);
-        const prophets = jsObject["prophets"];
-        console.log(prophets);
-        prophets.forEach(displayProphets)
+        const businesses = jsObject["businesses"];
+        console.log(businesses);
+        prophets.forEach(displayBusinesses)
     }
 };
+
+getBusinesses(requestURL);
+
+function displayBusinesses(item) {
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let address = document.createElement("p");
+    let phone = document.createElement("p");
+    let website = document.createElement("p");
+    let img = document.createElement("img");
+
+    // h2.textContent = item.name + ' ' + item.lastname;
+    h2.textContent = item.name;
+    address.textContent = item.address;
+    phone.textContent = item.phone;
+    website.textContent = item.website;
+    img.setAttribute("src", item.logo);
+    img.setAttribute("alt", item.name);
+
+
+    card.appendChild(h2);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+    card.appendChild(img);
+    
+    document.querySelector(".cards").appendChild(card);
+}
+
+// // HOW TO REMOVE
+// // -- put all cards in a div and hide that
+// // -- remove other thing first, then display new
+// // card.style.display = "none";
+// card.forEach((item) => {
+//     item.remove();
+// })
