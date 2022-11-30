28a2c7008f3ac1fed0cd44c94d8d125e25726350
sammieatkins <atk21007@byui.edu>
2022-11-30 13:05:01 -0700

    site plan and js

--- chamber2/js/main.js
+++ chamber2/js/main.js
@@ -23,32 +23,6 @@ if (now.getDay() == 1 || now.getDay() == 2) {
 }
 
 
-// date since last visit
-let currDate = Date.now();
-
-console.log(currDate);
-
-let visitMessage = document.querySelector("#visitMessage");
-let daysSince = document.querySelector("#daysSince");
-
-if (!localStorage.getItem("visitDate"))
-{
-    localStorage.setItem("visitDate", currDate);
-    visitMessage.textContent = "This is your first visit."
-}
-else
-{
-    let prevDate = localStorage.getItem("visitDate");
-    
-    let difference = currDate - prevDate;
-    // console.log(difference);
-    let daysBtwn = Math.floor(difference / 1000 / 60 / 60 / 24);
-    daysSince.textContent = daysBtwn;
-    // console.log(daysBtwn);
-    localStorage.setItem("visitDate", currDate);
-}
-
-
 // menu click event
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
