b36720726522f4cdf21a644c54327b06c207ee69
sammieatkins <atk21007@byui.edu>
2022-12-05 16:06:17 -0700

    json edit

--- final/js/temples.js
+++ final/js/temples.js
@@ -1,4 +1,4 @@
-const requestURL = "https://sammieatkins.github.io/wdd230/chamber2/data.json";
+const requestURL = "https://sammieatkins.github.io/wdd230/final/data.json";
 
 async function getBusinesses(requestURL) {
     const response = await fetch(requestURL);
@@ -6,27 +6,27 @@ async function getBusinesses(requestURL) {
     if (response.ok) {
         const jsObject = await response.json();
         console.log(jsObject);
-        const businesses = jsObject["businesses"];
-        console.log(businesses);
+        const temples = jsObject["temples"];
+        console.log(temples);
         
-        businesses.forEach(business => {
-            displayCards(business)
+        temples.forEach(temple => {
+            displayCards(temple)
         });
         
         document.getElementById("list").addEventListener("click", () => {
             document.querySelector(".cards").innerHTML = "";
             document.querySelector("#card").removeAttribute("disabled");
             document.querySelector("#list").setAttribute("disabled", true);
-            businesses.forEach(business => {                
-                displayTable(business)
+            temples.forEach(temple => {                
+                displayTable(temple)
             });
         });
         document.getElementById("card").addEventListener("click", () => {
             document.querySelector(".table").innerHTML = "";
             document.querySelector("#list").removeAttribute("disabled");     
             document.querySelector("#card").setAttribute("disabled", true);
-            businesses.forEach(business => {
-                displayCards(business)
+            temples.forEach(temple => {
+                displayCards(temple)
             });
         });        
     };
@@ -37,69 +37,66 @@ getBusinesses(requestURL);
 function displayCards(item) {
     let card = document.createElement("section");
 
-    let h2 = document.createElement("h2");
+    let name = document.createElement("h2");
     let address = document.createElement("p");
     let phone = document.createElement("p");
-    let website = document.createElement("a");
+    let services = document.createElement("p");
+    let history = document.createElement("p");
+    let closures = document.createElement("p");
     let img = document.createElement("img");
 
-    // h2.textContent = item.name + ' ' + item.lastname;
-    h2.textContent = item.name;
+    name.textContent = item.name;
     address.textContent = item.address;
     phone.textContent = item.phone;
-    website.setAttribute("href", item.website);
-    website.setAttribute("target", "_blank");
-    website.textContent = item.website;
-    img.setAttribute("src", `images/${item.logo}`);
+    services.textContent = item.services;
+    history.textContent = item.history;
+    closures.textContent = item.closures;
+    img.setAttribute("src", `../images/${item.img}`);
     img.setAttribute("alt", item.name);
 
-
-    card.appendChild(img);
-    card.appendChild(h2);
+    card.appendChild(name);
     card.appendChild(address);
     card.appendChild(phone);
-    card.appendChild(website);
+    card.appendChild(services);
+    card.appendChild(history);
+    card.appendChild(closures);
+    card.appendChild(img);
     
     document.querySelector(".cards").appendChild(card);
 }
 
 
-function displayTable(business) {
+function displayTable(temple) {
     largeQuery = window.matchMedia("(min-width: 64em)");
     let row = document.createElement("tr");
     if (largeQuery.matches) {
         let name = document.createElement("td");
         let address = document.createElement("td");
         let phone = document.createElement("td");
-        let website = document.createElement("td");
+        let services = document.createElement("p");
+        let history = document.createElement("p");
+        let closures = document.createElement("p");
         
-        name.textContent = business.name;
-        address.textContent = business.address;
-        phone.textContent = business.phone;
-        website.textContent = business.website;
+        name.textContent = temple.name;
+        address.textContent = temple.address;
+        phone.textContent = temple.phone;
+        services.textContent = item.services;
+        history.textContent = item.history;
+        closures.textContent = item.closures;
         
         row.appendChild(name);
         row.appendChild(address);
-        row.appendChild(phone);
-        row.appendChild(website);         
+        row.appendChild(phone);  
+        row.appendChild(services);
+        row.appendChild(history);
+        row.appendChild(closures);      
     }
     else {
-        let business_info = document.createElement("td");
+        let temple_info = document.createElement("td");
         // business_info.textContent = business.name + "\n" + business.address + "\n" + business.phone + "\n" + business.website;
-        business_info.textContent = `${business.name}\r\n${business.address}\r\n${business.phone}\r\n${business.website}`;
-        business_info.setAttribute('style', 'white-space: pre;');
-        row.appendChild(business_info);
+        temple_info.textContent = `${temple.name}\r\n${temple.address}\r\n${temple.phone}\r\n${temple.services}\r\n${temple.history}\r\n${temple.closures}`;
+        temple_info.setAttribute('style', 'white-space: pre;');
+        row.appendChild(temple_info);
     }
     document.querySelector(".table").appendChild(row);
 }
-// largeQuery.addEventListener("change", displayTable)
-// let largeQuery = window.matchMedia("(min-width: 64em)");
-
-
-// // HOW TO REMOVE
-// // -- put all cards in a div and hide that
-// // -- remove other thing first, then display new
-// // card.style.display = "none";
-// card.forEach((item) => {
-//     item.remove();
-// })
