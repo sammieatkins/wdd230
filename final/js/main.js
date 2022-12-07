2673b8fae1a11815ec849d712bde5349a8a390a7
sammieatkins <atk21007@byui.edu>
2022-12-06 20:26:34 -0700

    slideshow

--- final/js/main.js
+++ final/js/main.js
@@ -20,19 +20,33 @@ function makeFooter() {
 
     info.innerHTML = "Temple Inn & Suites | Samantha Atkins | WDD 230 | &copy; 2022";
 
+    // LOCATION
     locationLink.innerHTML = '<i class="fa-solid fa-location-dot"></i> 4800 Montgomery Ln #300, Bethesda, MD 20814';
     locationLink.setAttribute("href", "https://goo.gl/maps/j4mHpQSnSeXCGteF7");
     locationLink.setAttribute("target", "blank");
 
+    // SOCIALS
     facebook.innerHTML = '<i class="fa-brands fa-facebook-f"></i>';
     facebook.setAttribute("class", "social_icon");
+    facebook.setAttribute("href", "https://www.facebook.com/");
+    facebook.setAttribute("target", "blank");
+
     twitter.innerHTML = '<i class="fa-brands fa-twitter"></i>';
     twitter.setAttribute("class", "social_icon");
+    twitter.setAttribute("href", "https://www.twitter.com/");
+    twitter.setAttribute("target", "blank");
+
     instagram.innerHTML = '<i class="fa-brands fa-instagram"></i>';
     instagram.setAttribute("class", "social_icon");
+    instagram.setAttribute("href", "https://www.instagram.com/");
+    instagram.setAttribute("target", "blank");
+
     pinterest.innerHTML = '<i class="fa-brands fa-pinterest-p"></i>';
     pinterest.setAttribute("class", "social_icon");
+    pinterest.setAttribute("href", "https://www.pinterest.com/");
+    pinterest.setAttribute("target", "blank");
 
+    // APPENDING
     location.appendChild(locationLink);
 
     socialIcons.setAttribute("class", "footer_social_icons");
