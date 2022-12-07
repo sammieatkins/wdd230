61b9ded34dd329c2c0c57456c5f8a74be2ee296c
sammieatkins <atk21007@byui.edu>
2022-12-06 20:55:29 -0700

    set up all pages and added questions

--- final/js/home.js
+++ final/js/home.js
@@ -43,6 +43,22 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+// AUTOMATIC SLIDESHOW
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
 
 // TEMPLE SPOTLIGHT
 const requestURL = "https://sammieatkins.github.io/wdd230/final/data.json";
