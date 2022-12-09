59aee4a4128c8a61b53c61c3f8809cea3843d2bc
sammieatkins <atk21007@byui.edu>
2022-12-09 14:40:15 -0700

    json change

--- final/js/home.js
+++ final/js/home.js
@@ -48,21 +48,40 @@ const getWeather = async () => {
 getWeather();
 
 
+
+// AUTOMATIC SLIDESHOW
+let slideIndex = 0;
+automaticSlides();
+
+function automaticSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(automaticSlides, 4000);
+}
+
 // SLIDESHOW
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+clickSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  clickSlides(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  clickSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function clickSlides(n) {
+  // clearTimeout(automaticSlides);
+
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
@@ -78,21 +97,10 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-// AUTOMATIC SLIDESHOW
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+prev.addEventListener("click", () => {plusSlides(-1);});
+next.addEventListener("click", () => {plusSlides(1);});
 
 
 // TEMPLE SPOTLIGHT
